9572d9fa41dc78abb1715917f804b3cb86c2f6af
100rabh999 <saurabhsingh2001.sk@gmail.com>
2024-04-19 10:49:52 +0530

    first code

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -75,7 +75,7 @@ const LoginPage = ({ role }) => {
 
     const guestModeHandler = () => {
         const password = "zxc"
-
+console.log("data",role);
         if (role === "Admin") {
             const email = "yogendra@12"
             const fields = { email, password }
